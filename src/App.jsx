4320fc412da73e3241bb8c173fb309bca6af5c45
Jintae Song <w.jinttao@gmail.com>
2025-02-18 14:22:58 +0900

    Refactor: modify directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { setWelcomeToastConfig } from './welcomeToast';
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { setWelcomeToastConfig } from "./welcomeToast";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     setWelcomeToastConfig();
@@ -30,11 +30,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
