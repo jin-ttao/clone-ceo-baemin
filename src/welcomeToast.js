504f4e55d0f9c709e21ba69beb2b2c6a86a0a6f5
Jintae Song <w.jinttao@gmail.com>
2025-02-18 14:23:12 +0900

    Refactor: apply prettier

--- src/welcomeToast.js
+++ src/welcomeToast.js
@@ -2,9 +2,9 @@ function setWelcomeToastConfig() {
   if (!window.welcometoastConfig) {
     window.welcometoastConfig = {
       apiKey: import.meta.env.VITE_WELCOME_TOAST_API_KEY,
-      init: function() {
+      init: function () {
         window.welcometoast.getProject(window.welcometoastConfig.apiKey);
-      }
+      },
     };
   }
 }
